c9cc7e89d361df87dabe64a4740cc16ce330d739
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 11:49:40 -0300

    Add InsertProduct

--- RepositoryPattern/DapperSqlProductRepository.cs
+++ RepositoryPattern/DapperSqlProductRepository.cs
@@ -1,9 +1,17 @@
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
 namespace RepositoryPattern
 {
     public class DapperSqlProductRepository : SqlRepository, IProductRepository
     {
         public DapperSqlProductRepository() { }
 
-        
+        public void InsertProduct(Product product)
+        {
+            var command = $"INSERT INTO Product (Id, Name) VALUES (@Id, @Name)";
+
+            Databaseee.Execute(command, product);
+        }
     }
 }
