3bb77e65a7d0c498c097bcf823bb706ad5a92908
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 14:02:06 -0300

    Fix methods

--- DesignPatterns/DependencyResolver.cs
+++ DesignPatterns/DependencyResolver.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace DesignPatterns
 {
@@ -19,6 +20,21 @@ namespace DesignPatterns
                 return new DependencyResolver(
                     _serviceProvider);
             }
+        }
+
+        public static void SetLocatorProvider(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _currentServiceProvider.GetService(serviceType);
+        }
+
+        public TService GetService<TService>()
+        {
+            return _currentServiceProvider.GetService<TService>();
         }
     }
 }
