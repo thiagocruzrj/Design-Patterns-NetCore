2b344a70937ff1343260acbb531d9d9e745fa567
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 14:05:39 -0300

    Create ProductHelper.cs

---
+++ DesignPatterns/ServiceLocator/ProductHelper.cs
@@ -0,0 +1,21 @@
+﻿using DesignPatterns.Repository;
+using Microsoft.Extensions.Configuration;
+
+namespace DesignPatterns.ServiceLocator
+{
+    public static class ProductHelper
+    {
+        public static IProductRepository ProductRepository()
+        {
+            var repository = DependencyResolver.Current.GetService<IProductRepository>();
+            var configuration = DependencyResolver.Current.GetService<IConfiguration>();
+
+            if(repository == null)
+            {
+                repository = new DapperSqlProductRepository(configuration);
+            }
+
+            return repository;
+        }
+    }
+}
