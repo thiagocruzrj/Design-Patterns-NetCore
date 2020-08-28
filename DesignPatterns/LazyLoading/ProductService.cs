a9f3cfc0d0acd4e554331327d33fbade1677f615
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 14:16:42 -0300

    Create ProductService.cs

---
+++ DesignPatterns/LazyLoading/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.Repository;
+
+namespace DesignPatterns.LazyLoading
+{
+    public class ProductService
+    {
+        private IProductRepository _productRepository;
+        public IProductRepository ProductRepository
+        {
+            get
+            {
+                if (_productRepository == null)
+                    _productRepository = DependencyResolver.Current.GetService<IProductRepository>();
+                return _productRepository;
+            }
+        }
+    }
+}
