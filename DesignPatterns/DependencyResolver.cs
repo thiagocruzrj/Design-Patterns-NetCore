84f58fcf374b9a88a2a0043961032bb80a52ee20
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 13:59:27 -0300

    Add DependencyResolver class

---
+++ DesignPatterns/DependencyResolver.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DesignPatterns
+{
+    public sealed class DependencyResolver
+    {
+        private ServiceProvider _currentServiceProvider;
+        private static ServiceProvider _serviceProvider;
+
+        public DependencyResolver( ServiceProvider currentServiceProvider)
+        {
+            _currentServiceProvider = currentServiceProvider;
+        }
+
+        public static DependencyResolver Current
+        {
+            get
+            {
+                return new DependencyResolver(
+                    _serviceProvider);
+            }
+        }
+    }
+}
