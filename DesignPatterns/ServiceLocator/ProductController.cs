1a15165f0b0257ec34359ea3d67f7fe84aed1d0c
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 14:03:20 -0300

    Update ProductController.cs

--- DesignPatterns/ServiceLocator/ProductController.cs
+++ DesignPatterns/ServiceLocator/ProductController.cs
@@ -3,14 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DesignPatterns.ServiceLocator
 {
-    public class ProductController : Controller
+    public sealed class ProductController : Controller
     {
         private IProductRepository ProductRepository
         {
             get
             {
-                return DependencyResolver.Current
+                return DependencyResolver.Current.GetService<IProductRepository>();
             }
+        }
+
+        public IActionResult Index()
+        {
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "product 1"
+            };
+
+            ProductRepository.Insert(product);
+
+            return Content("Service Locator Pattern");
         }
     }
 }
