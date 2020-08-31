a889b40e5b1391b008d9f52ed0e498f9d7fd5da7
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 19:10:08 -0300

    Create Facade.cs

---
+++ DesignPatterns/Facade/Facade.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns.Facade
+{
+    public class Facade
+    {
+        private Logger _logger;
+        private Payment _payment;
+        private SecurityInfo _securityInfo;
+        private Transaction _transaction;
+
+        public Facade()
+        {
+            _logger = new Logger();
+            _payment = new Payment();
+            _securityInfo = new SecurityInfo();
+            _transaction = new Transaction();
+        }
+
+        public bool Pay(string userId, double amount)
+        {
+
+        }
+    }
+}
