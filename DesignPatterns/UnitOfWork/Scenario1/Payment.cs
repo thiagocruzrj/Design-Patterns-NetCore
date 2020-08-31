6e44c0bcf1b348fbce974ee2d42aa7c3d3c39b68
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 11:16:37 -0300

    Create Payment.cs

---
+++ DesignPatterns/UnitOfWork/Scenario1/Payment.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.UnitOfWork.Scenario1
+{
+    public class Payment
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public int? ProductId { get; set; }
+    }
+}
