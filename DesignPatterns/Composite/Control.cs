f7b1df1f2a87f11191c7ff6b1c19db681d9498be
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:17:39 -0300

    Create Control.cs

---
+++ DesignPatterns/Composite/Control.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Composite
+{
+    public class Control : IControl
+    {
+        public int Id { get; set; }
+
+        public string Reader()
+        {
+            return $"Control {Id}";
+        }
+    }
+}
