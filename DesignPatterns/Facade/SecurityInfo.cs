8b5851618570a15156c165d001a594acd6b0c662
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 19:07:21 -0300

    Create SecurityInfo.cs

---
+++ DesignPatterns/Facade/SecurityInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Facade
+{
+    public sealed class SecurityInfo
+    {
+        public string GetFromUserId(string userId)
+        {
+            return "ID232R54";
+        }
+    }
+}
