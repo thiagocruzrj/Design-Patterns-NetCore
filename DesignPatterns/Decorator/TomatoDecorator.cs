36594bcfa8e63231013678d4332f828e43a176b0
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:50:50 -0300

    Create TomatoDecorator.cs

---
+++ DesignPatterns/Decorator/TomatoDecorator.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public class TomatoDecorator : PizzaDecorator
+    {
+        public TomatoDecorator(Pizza pizza) : base(pizza)
+        {
+            Description = "tomato";
+        }
+
+        public override double CalculateCost()
+        {
+            return base.CalculateCost() + 0.25;
+        }
+
+        public override string GetDescription()
+        {
+            return $"{base.GetDescription()}, {Description}";
+        }
+    }
+}
