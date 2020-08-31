d56bdfc27c17d632949b603e9041baa758647901
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:01:54 -0300

    Create CalcRectangle.cs

---
+++ DesignPatterns/Adapter/CalcRectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Adapter
+{
+    public sealed class CalcRectangle
+    {
+        public int CalculateArea(Rectangle rectangle)
+        {
+            int area = (rectangle.Width * rectangle.Height);
+
+            return area;
+        }
+    }
+}
