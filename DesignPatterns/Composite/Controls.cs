cb5b97c8a8e1012c866debc3cee8e3233a81fba7
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:17:41 -0300

    Create Controls.cs

---
+++ DesignPatterns/Composite/Controls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Composite
+{
+    public class Controls : List<IControl>, IControl
+    {
+        public int Id { get; set; }
+
+        public string Reader()
+        {
+            var sb = new StringBuilder();
+            foreach (var control in this)
+            {
+                sb.Append($"{Id}: control: {control.Reader()}</br>");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
