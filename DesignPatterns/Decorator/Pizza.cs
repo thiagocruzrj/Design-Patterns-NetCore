9869a69bf4e13961d33192a95a37c9a2bdc16611
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:16:48 -0300

    Create Pizza.cs

---
+++ DesignPatterns/Decorator/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public abstract class Pizza
+    {
+        public string Description { get; set; }
+        public abstract string GetDescription();
+        public abstract double CalculateCost();
+    }
+}
