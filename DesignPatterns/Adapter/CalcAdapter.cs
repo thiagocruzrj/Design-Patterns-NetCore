54bfec2f462b33949e7de444193e4b4618d7474b
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:01:56 -0300

    Create CalcAdapter.cs

---
+++ DesignPatterns/Adapter/CalcAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Adapter
+{
+    public class CalcAdapter
+    {
+        public int CalculateArea(Square square)
+        {
+            var calcRectangle = new CalcRectangle();
+            var rectangle = new Rectangle { Width = square.Side, Height = square.Side };
+            var area = calcRectangle.CalculateArea(rectangle);
+
+            return area;
+        }
+    }
+}
