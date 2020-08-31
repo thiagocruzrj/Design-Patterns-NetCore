fa890bc318536526aad0be726dd2066825f0408f
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 19:15:43 -0300

    Update Facade.cs

--- DesignPatterns/Facade/Facade.cs
+++ DesignPatterns/Facade/Facade.cs
@@ -17,7 +17,22 @@
 
         public bool Pay(string userId, double amount)
         {
+            _logger.Log("PaymentStart");
+            var result = false;
+            var securityId = _securityInfo.GetFromUserId(userId);
+            _logger.Log($"Security id {securityId} for user id {userId}");
+            _transaction.Start();
+            _transaction.Do(() =>
+            {
+                result = _payment.Pay(securityId, amount);
+                _logger.Log($"Paying {amount} for security id {securityId}");
+            });
 
+            _transaction.End();
+            _logger.Log("Transaction end");
+            _logger.Log($"Payment result {result}");
+
+            return result;
         }
     }
 }
