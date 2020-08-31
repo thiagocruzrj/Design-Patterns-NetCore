49ef87a8eee0b43251c7fd9d2fa382a5ae3a633d
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 19:23:11 -0300

    Create FacadeController.cs

---
+++ DesignPatterns/Facade/FacadeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Facade
+{
+    public sealed class FacadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var facade = new Facade();
+            var result = facade.Pay("3232", 45.330);
+
+            return Content("With facade");
+        }
+    }
+}
