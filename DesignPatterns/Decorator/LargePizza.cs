de4605c8f89d9efefb01a713b28344278a6181ff
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:18:01 -0300

    Create LargePizza.cs

---
+++ DesignPatterns/Decorator/LargePizza.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public sealed class LargePizza : Pizza
+    {
+        public LargePizza()
+        {
+            Description = "Large Pizza";
+        }
+        public override double CalculateCost()
+        {
+            return 45.00;
+        }
+
+        public override string GetDescription()
+        {
+            return Description;
+        }
+    }
+}
