a01cd4c3c0de76b55115c05ad59609f1b38576b9
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 11:16:35 -0300

    Create IRegisterUnitOfWork.cs

---
+++ DesignPatterns/UnitOfWork/Scenario1/IRegisterUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using DesignPatterns.Repository;
+
+namespace DesignPatterns.UnitOfWork.Scenario1
+{
+    public interface IRegisterUnitOfWork
+    {
+        void Insert(Product product, Payment payment);
+        IProductRepository ProductRepository { get; }
+        IPaymentRepository PaymentRepository { get; }
+    }
+}
