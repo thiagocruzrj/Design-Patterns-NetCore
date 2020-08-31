a28e5e920708ee45a526b3b0387907e3963467ed
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:36:20 -0300

    Create CheeseDecorator.cs

---
+++ DesignPatterns/Decorator/CheeseDecorator.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public sealed class CheeseDecorator : PizzaDecorator
+    {
+        public CheeseDecorator(Pizza pizza) : base(pizza)
+        {
+            Description = "cheese";
+        }
+
+        public override double CalculateCost()
+        {
+            return base.CalculateCost() + 1.25;
+        }
+
+        public override string GetDescription()
+        {
+            return $"{base.CalculateCost()}, {Description}";
+        }
+    }
+}
