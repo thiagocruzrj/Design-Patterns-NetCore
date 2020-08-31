97eb149cdd2189cedab7ba54e3ab98c5c6496fef
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 11:16:30 -0300

    Create DapperSqlPaymentRepository.cs

---
+++ DesignPatterns/UnitOfWork/Scenario1/DapperSqlPaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns.Repository;
+using Microsoft.Extensions.Configuration;
+
+namespace DesignPatterns.UnitOfWork.Scenario1
+{
+    public sealed class DapperSqlPaymentRepository : DapperSqlRepository<Payment>, IPaymentRepository
+    {
+        public DapperSqlPaymentRepository(IConfiguration configuration) : base(configuration)
+        {
+            TableName = "Payment";
+        }
+    }
+}
