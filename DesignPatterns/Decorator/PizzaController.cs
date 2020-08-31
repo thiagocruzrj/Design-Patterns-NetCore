6617f6a1f0ad5e7032ab54fefc17a1650e1e6cba
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:53:45 -0300

    Create PizzaController.cs

---
+++ DesignPatterns/Decorator/PizzaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Decorator
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pizza = new LargePizza();
+            var cheese = new CheeseDecorator(pizza);
+            var tomato = new TomatoDecorator(cheese);
+
+            return Content($"Decorato Pattern Pizza: {tomato.GetDescription()} - {tomato.CalculateCost()}");
+        }
+    }
+}
