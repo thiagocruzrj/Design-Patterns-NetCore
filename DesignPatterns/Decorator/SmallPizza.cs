fc72d0b8101ea78d63314692bf0cc5f92d116392
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 12:19:00 -0300

    Create SmallPizza.cs

---
+++ DesignPatterns/Decorator/SmallPizza.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public class SmallPizza : Pizza
+    {
+        public SmallPizza()
+        {
+            Description = "Small pizza";
+        }
+        public override double CalculateCost()
+        {
+            return 20.00;
+        }
+
+        public override string GetDescription()
+        {
+            return Description;
+        }
+    }
+}
