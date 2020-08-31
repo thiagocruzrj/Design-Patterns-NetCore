fdec1367887395b2c8a6e0282611159fbbcb6644
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:04:44 -0300

    Create AdapterController.cs

---
+++ DesignPatterns/Adapter/AdapterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Adapter
+{
+    public sealed class AdapterController : Controller
+    {
+        public IActionResult Index()
+        {
+            var square = new Square() { Side = 30 };
+            var calc = new CalcAdapter();
+            var area = calc.CalculateArea(square);
+
+            return Content($"ADAPTER PATTERN RESULT area {square.Side}: {area}");
+        }
+    }
+}
