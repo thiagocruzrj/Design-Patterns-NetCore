b4440ad1b2a06b740939d02669bfc45f4a5fb7db
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 11:16:42 -0300

    Create RegisterUnitOfWorkController.cs

---
+++ DesignPatterns/UnitOfWork/Scenario1/RegisterUnitOfWorkController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.UnitOfWork.Scenario1
+{
+    public class RegisterUnitOfWorkController : Controller
+    {
+        private readonly IRegisterUnitOfWork _unitOfWork;
+
+        public RegisterUnitOfWorkController(IRegisterUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
