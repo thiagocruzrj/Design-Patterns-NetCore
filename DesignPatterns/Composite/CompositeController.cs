1412b3490f24691a8e013b827673522a338f3fc2
Thiago Cruz <thagocruz@gmail.com>
2020-08-31 18:29:57 -0300

    Create CompositeController.cs

---
+++ DesignPatterns/Composite/CompositeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Composite
+{
+    public sealed class CompositeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var nodes = new Controls() { Id = 1 };
+            nodes.Add(new Control() { Id = 2 });
+            nodes.Add(new Control() { Id = 3 });
+            IControl control = nodes;
+
+            return Content($"Composite:</br> {control.Render()}");
+        }
+    }
+}
