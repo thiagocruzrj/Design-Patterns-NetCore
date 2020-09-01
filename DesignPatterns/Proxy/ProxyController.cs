1267ee6a84cf247eeeae1523be4aa7f5321a6cf8
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 10:54:38 -0300

    Update ProxyController.cs

--- DesignPatterns/Proxy/ProxyController.cs
+++ DesignPatterns/Proxy/ProxyController.cs
@@ -6,7 +6,12 @@ namespace DesignPatterns.Proxy
     {
         public IActionResult Index(int id = 16)
         {
+            var user = new User() { Age = id };
+            var proxy = new CalcProxy(user);
+            var result = proxy.Calculate();
+            var message = proxy.Message;
 
+            return Content($"result from Proxy: {result} of age {id}: {message}");
         }
     }
 }
