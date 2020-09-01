f0754035515d2da823bd99fa6a2ed37a4ef2d91c
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 12:11:45 -0300

    Create StrategyController.cs

---
+++ DesignPatterns/Strategy/StrategyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Strategy
+{
+    public class StrategyController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<string>() { "thiago", "isabella", "suzi" };
+
+            var sort = new Strategy(new DescendingSortStrategy());
+            sort.Sort(list);
+            var sortAs = new Strategy(new AscendingSortStrategy());
+            sortAs.Sort(list);
+            var sortBu = new Strategy(new BubbleSortStrategy());
+            sortBu.Sort(list);
+
+            return Content("Strategy");
+        }
+    }
+}
