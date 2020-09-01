48dcc262ae53e38f8503143c34a8f4a66dcab916
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 14:33:04 -0300

    Create Bus.cs

---
+++ DesignPatterns/Factory/Bus.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Factory
+{
+    public sealed class Bus : ITransport
+    {
+        public string Build()
+        {
+            return "Bus transport";
+        }
+    }
+}
