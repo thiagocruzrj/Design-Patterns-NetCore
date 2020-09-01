e8c45dcd6fc3b3f0739c94424c150fd9104bb1e4
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 10:52:58 -0300

    Create CalcProxy.cs

---
+++ DesignPatterns/Proxy/CalcProxy.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Proxy
+{
+    public class CalcProxy : ICalc
+    {
+        private User _user;
+        private Calc _calc;
+
+        public CalcProxy(User user)
+        {
+            _user = user;
+            _calc = new Calc();
+        }
+
+        public string Message { get; set; }
+
+        public double Calculate()
+        {
+            var age = 18;
+
+            if(_user.Age < age)
+            {
+                Message = $"Must be greater than {age}";
+                return 0;
+            }
+
+            return _calc.Calculate();
+        }
+    }
+}
