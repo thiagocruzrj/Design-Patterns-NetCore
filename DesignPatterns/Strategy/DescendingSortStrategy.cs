805acb7e436c8a53f894363ff04ac63b84cda668
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 12:11:41 -0300

    Create DescendingSortStrategy.cs

---
+++ DesignPatterns/Strategy/DescendingSortStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Strategy
+{
+    public class DescendingSortStrategy : ISortStrategy
+    {
+        public void Sort(List<string> list)
+        {
+            list = list.OrderByDescending(x => x).ToList();
+        }
+    }
+}
