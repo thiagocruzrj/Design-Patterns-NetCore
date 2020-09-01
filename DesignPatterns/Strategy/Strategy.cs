1ce6626979e0b5fd9d79369463c50aafc6ec8b70
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 12:11:43 -0300

    Create Strategy.cs

---
+++ DesignPatterns/Strategy/Strategy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Strategy
+{
+    public class Strategy
+    {
+        private readonly ISortStrategy _strategy;
+
+        public Strategy(ISortStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public List<string> Sort(List<string> list)
+        {
+            _strategy.Sort(list);
+
+            return list;
+        }
+    }
+}
