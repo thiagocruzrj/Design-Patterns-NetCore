74cb869fd3265952e3f20827c8114a490c643b4b
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 11:36:10 -0300

    Create SingletonExtension.cs

---
+++ DesignPatterns/Singleton/SingletonExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class SingletonExtension
+    {
+        public static void UseSingletion(this IServiceCollection services)
+        {
+            services.AddSingleton<ILog, DatabaseLog>();
+        }
+    }
+}
