c754b15d265dcd3cab4dc706b7e81da5256edeb8
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 12:11:38 -0300

    Create BubbleSortStrategy.cs

---
+++ DesignPatterns/Strategy/BubbleSortStrategy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Strategy
+{
+    public class BubbleSortStrategy : ISortStrategy
+    {
+        public void Sort(List<string> list) { }
+    }
+}
