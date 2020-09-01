05d26873e3728c7c703fe7a69740a7e981aad6f7
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 15:28:26 -0300

    Create Button.cs

---
+++ DesignPatterns/Flyweight/Button.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Flyweight
+{
+    public sealed class Button : Control
+    {
+        public Button()
+        {
+            Tag = Tags.Button;
+        }
+
+        public override string Create(string text)
+        {
+            return $"<input type='button' value='{text}' /> => {text}";
+        }
+    }
+}
