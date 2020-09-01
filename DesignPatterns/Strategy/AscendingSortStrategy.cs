ba23dcadfdba1feafafa202a4d736b6a75560978
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 12:11:34 -0300

    Create AscendingSortStrategy.cs

---
+++ DesignPatterns/Strategy/AscendingSortStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Strategy
+{
+    public class AscendingSortStrategy : ISortStrategy
+    {
+        public void Sort(List<string> list)
+        {
+            list = list.OrderBy(x => x).ToList();
+        }
+    }
+}
