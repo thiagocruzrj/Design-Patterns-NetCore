56d89437489888abbbdabeba16b6c6b9a1c17f5b
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 14:33:16 -0300

    Create FactoryController.cs

---
+++ DesignPatterns/Factory/FactoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Factory
+{
+    public class FactoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var types = new List<string>() { "Car", "Bus", "Truck" };
+            var transports = new List<ITransport>();
+            types.ForEach(x => transports.Add(FactoryTransport.CreateInstance(x)));
+
+            foreach (var transport in transports)
+            {
+                var result = transport.Build();
+            }
+
+            return Content("Facotory used");
+        }
+    }
+}
