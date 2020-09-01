972f06334509cd3a41026bb1689b92f73ebd893d
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 10:52:53 -0300

    Create Calc.cs

---
+++ DesignPatterns/Proxy/Calc.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Proxy
+{
+    public class Calc : ICalc
+    {
+        public string Message { get; set; }
+
+        public double Calculate()
+        {
+            Message = "Calculated";
+            return 32 * 9;
+        }
+    }
+}
