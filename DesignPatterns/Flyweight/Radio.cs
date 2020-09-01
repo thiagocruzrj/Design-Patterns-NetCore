a714ca41e3475033a2e7900241059bcfde7163f3
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 15:28:30 -0300

    Create Radio.cs

---
+++ DesignPatterns/Flyweight/Radio.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Flyweight
+{
+    public sealed class Radio : Control
+    {
+        public Radio()
+        {
+            Tag = Tags.Radio;
+        }
+
+        public override string Create(string text)
+        {
+            return $"<input type='radio' value='{text}' /> => {text}";
+        }
+    }
+}
