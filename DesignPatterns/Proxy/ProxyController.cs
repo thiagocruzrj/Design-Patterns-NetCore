9ab8d585d9d17cbcfd829fdb04169d9ec7dba477
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 10:53:00 -0300

    Create ProxyController.cs

---
+++ DesignPatterns/Proxy/ProxyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Proxy
+{
+    public class ProxyController : Controller
+    {
+        public IActionResult Index(int id = 16)
+        {
+
+        }
+    }
+}
