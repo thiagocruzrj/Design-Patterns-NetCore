a6b1b3e06130c65bb8a8daa8eae21c3921a3258d
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 14:04:42 -0300

    Create ChainController.cs

---
+++ DesignPatterns/ChainOfResponsability/ChainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.ChainOfResponsability
+{
+    public class ChainController : Controller
+    {
+        public IActionResult Index()
+        {
+            var seller = new Seller("Thiago", 30);
+            var localManager = new Seller("Isabella", 50);
+            var regionalManager = new Seller("Suzi", 70);
+
+            seller.SetSuperior(localManager);
+            localManager.SetSuperior(regionalManager);
+
+            var result = seller.ApplyDiscount(40);
+            if(result.Approved)
+            {
+                var mesage = $"Seller {result.SellerName} approved {result.Value}!";
+            }
+
+            return Content("Chain applied");
+        }
+    }
+}
