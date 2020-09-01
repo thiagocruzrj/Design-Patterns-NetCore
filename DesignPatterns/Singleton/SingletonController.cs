658853fa91f81ca372ccb6683f84bcbcaa3bc455
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 11:36:11 -0300

    Create SingletonController.cs

---
+++ DesignPatterns/Singleton/SingletonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class SingletonController : Controller
+    {
+        public IActionResult Index()
+        {
+            Log.Instance.LogException(new Exception("test"));
+
+            return Content("Singleton");
+        }
+    }
+}
