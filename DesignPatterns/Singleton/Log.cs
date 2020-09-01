f9906fbd0a07638ec15a0a88c344638de5f6ae70
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 11:36:08 -0300

    Create Log.cs

---
+++ DesignPatterns/Singleton/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class Log
+    {
+        private static volatile ILog instance;
+        private static object syncRoot = new Object();
+
+        public Log() { }
+
+        public static ILog Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock(syncRoot)
+                    {
+                        if(instance == null)
+                        {
+                            instance = DependencyResolver.Current.GetService<ILog>();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
