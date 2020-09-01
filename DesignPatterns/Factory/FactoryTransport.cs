db3ae47ddb2f2caebeadcb14c9c3859256769e61
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 14:33:12 -0300

    Create FactoryTransport.cs

---
+++ DesignPatterns/Factory/FactoryTransport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Factory
+{
+    public static class FactoryTransport
+    {
+        public static ITransport CreateInstance(string name)
+        {
+            return Activator.CreateInstance(Type.GetType($"{name}")) as ITransport;
+        }
+    }
+}
