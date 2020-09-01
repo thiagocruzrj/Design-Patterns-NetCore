aac475690549e5b263fe2267cd49f4898cf43d4e
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 14:04:38 -0300

    Create Discount.cs

---
+++ DesignPatterns/ChainOfResponsability/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.ChainOfResponsability
+{
+    public class Discount
+    {
+        public string SellerName { get; set; }
+        public double Value { get; set; }
+        public bool Approved { get; set; }
+    }
+}
